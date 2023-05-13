4698f90522dcb5be59624098276107e1bb079236
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-14 03:38:47 +1000

    rendering images and css

--- starter/src/Gallery.jsx
+++ starter/src/Gallery.jsx
@@ -12,8 +12,44 @@ const Gallery = () => {
       return result.data;
     },
   });
-  console.log(response);
-  return <h2>Gallery</h2>;
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h4>there was an console.error</h4>
+      </section>
+    );
+  }
+  const results = response.data.results;
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No such search exist</h4>
+      </section>
+    );
+  }
+  // console.log(response);
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className="img"
+          />
+        );
+      })}
+    </section>
+  );
 };
 
 export default Gallery;
