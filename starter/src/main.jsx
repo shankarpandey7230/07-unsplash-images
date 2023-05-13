7deb848f6fab1e3cfe2eeb41c6abbb5594ad5542
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-14 03:43:53 +1000

    installing react-query- devtools

--- starter/src/main.jsx
+++ starter/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 import { AppProvider } from "./context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <AppProvider>
     <QueryClientProvider client={queryClient}>
       <App />
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </AppProvider>
 );
