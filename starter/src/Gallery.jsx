81897a7d921443fbc1c2775e38ae055b484155ad
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-14 03:07:46 +1000

    installing and setting up the query

--- starter/src/Gallery.jsx
+++ starter/src/Gallery.jsx
@@ -1,4 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const url =
+  "https://api.unsplash.com/search/photos?client_id=7J-v3FjJ5PlVWeD9-z5zNQpZ4UsS4I4oEaVt96kb6Z4&query=office";
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+
+      return result.data;
+    },
+  });
+  console.log(response);
   return <h2>Gallery</h2>;
 };
 
--- starter/src/main.jsx
+++ starter/src/main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { AppProvider } from "./Context.jsx";
+import App from "./App";
 import "./index.css";
+import { AppProvider } from "./context";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AppProvider>
+  <AppProvider>
+    <QueryClientProvider client={queryClient}>
       <App />
-    </AppProvider>
-  </React.StrictMode>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
+  </AppProvider>
 );
